a52cb3445b0805e5805b6fe6899b2605da0faf2a
Neezar <rforlyffe@gmaicl.com>
2020-11-29 18:13:36 +0800

    init apollo

---
+++ src/lib/apollo.ts
@@ -0,0 +1,49 @@
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { useMemo } from 'react'
+
+let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
+
+function createApolloClient() {
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('token')
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    }
+  })
+  const httpLink = new HttpLink({
+    uri: 'http://localhost:8001/graphql',
+    credentials: 'include',
+  })
+  return new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  })
+}
+
+export function initializeApollo(inititalState: any = null) {
+  const _apolloClient = apolloClient ?? createApolloClient()
+
+  if (inititalState) {
+    _apolloClient.cache.restore(inititalState)
+  }
+
+  if (typeof window === 'undefined') return _apolloClient
+
+  if (!apolloClient) apolloClient = _apolloClient
+
+  return _apolloClient
+}
+
+export function useApollo(initialState: any) {
+  const store = useMemo(() => initializeApollo(initialState), [initialState])
+  return store
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import App from 'next/app'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/core'
 import { themeDark, themeLight } from '../lib/theme'
 import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { useApollo } from '../lib/apollo'
+import { ApolloProvider } from '@apollo/client'
 
 const Apps = ({ Component, pageProps }: AppProps) => {
+  const apolloClient = useApollo(pageProps.initialApolloState)
+  const [darkState, setDarkState] = useState(false)
+
+  const handleThemeChange = () => {
+    setDarkState(!darkState)
+  }
+
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side')
     if (!jssStyles && jssStyles!.parentNode) {
@@ -13,10 +22,12 @@ const Apps = ({ Component, pageProps }: AppProps) => {
     }
   }, [])
   return (
-    <ThemeProvider theme={false ? themeDark : themeLight}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={apolloClient}>
+      <ThemeProvider theme={darkState ? themeDark : themeLight}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
